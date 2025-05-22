de1a7ea7aa2ba098b25bcb6cb96ae8e9c768fbba
axelmaya69 <axeldbmaya96@gmail.com>
2025-05-21 22:10:51 -0600

    Agregando

---
+++ product/src/main/java/com/product/repository/IProducto.java
@@ -0,0 +1,9 @@
+package com.product.repository;
+
+import com.product.entity.Producto;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface IProducto extends JpaRepository<Producto,Integer> {
+}
