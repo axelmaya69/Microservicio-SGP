77f76472ac8b6d8da6ccdd6c74880c4e63add052
axelmaya69 <axeldbmaya96@gmail.com>
2025-05-22 22:37:14 -0600

    Agregando

--- user/src/main/java/com/user/controller/ControllerUser.java
+++ user/src/main/java/com/user/controller/ControllerUser.java
@@ -1,9 +1,12 @@
 package com.user.controller;
 
+import com.user.entity.User;
 import com.user.service.IUserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/")
@@ -11,6 +14,38 @@ public class ControllerUser {
 
     @Autowired
     IUserService userService;
+
+    @GetMapping("/get")
+    public ResponseEntity<?> obtenerUsers(){
+        List<User> users =userService.obtenerUsers();
+        return ResponseEntity.ok(users);
+    }
+
+    @GetMapping("/get/{id}")
+    public ResponseEntity<?> obtenerUser(@PathVariable int id){
+        User user = userService.obtenerUser(id);
+        return ResponseEntity.ok("Obtenido");
+    }
+
+    @PostMapping("/post")
+    public ResponseEntity<?> ingresarUser(@RequestBody User user){
+        User putUser = userService.crearUser(user);
+        return ResponseEntity.ok("creado");
+    }
+
+
+    @PutMapping("/put/{id}")
+    public ResponseEntity<?> updateUser(@PathVariable int id, @RequestBody User user){
+        User userUp = userService.actualizarUser(id,user);
+        return ResponseEntity.ok("Editado");
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<?> deleteUser(@PathVariable int id){
+        userService.eliminarUser(id);
+        return ResponseEntity.ok("Eliminado");
+    }
+
 
 
 
