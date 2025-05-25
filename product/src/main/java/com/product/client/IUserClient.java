2cecd7586345728179753550399e111ae7c981f4
axelmaya69 <axeldbmaya96@gmail.com>
2025-05-24 22:51:32 -0600

    Agregando

--- product/src/main/java/com/product/client/IUserClient.java
+++ product/src/main/java/com/product/client/IUserClient.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 
-@FeignClient(name = "microservice-user",url = "localhost:8080/api/user")
+@FeignClient(name = "microservice-user",url = "localhost:8090/api/user")
 public interface IUserClient {
 
     @GetMapping("/search-my-product/{id}")
